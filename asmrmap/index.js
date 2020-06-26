3c5e1cf30482e0b90c551bb527e4cfb947af4230
F1ks3r <f1ks3r@yandex.ru>
2020-06-27 00:33:13 +0300

    save

--- asmrmap/index.js
+++ asmrmap/index.js
@@ -5,52 +5,53 @@ const YT_API_TOKEN = 'AIzaSyA2p8emllzwOt7hs2EwSnJr-VqRXMoCT-o'
 // const GOOGLE_CLIENT_SECRET = '709L5MKDaMDKM6ERh5mPzR8M'
 // const url1 = `https://www.googleapis.com/youtube/v3/search?part=snippet&maxResults=25&q=surfing&key=${YT_API_TOKEN}`
 // TODO "поменять с forUsername на ID"
-const url2 = (channelName = 'Rambalac') => `https://www.googleapis.com/youtube/v3/channels?part=contentDetails&forUsername=${channelName}&key=${YT_API_TOKEN}`
-const url3 = id => `https://www.googleapis.com/youtube/v3/playlistItems?part=snippet&maxResults=1000&playlistId=${id}&key=${YT_API_TOKEN}`
-
-const dataModel = JSON.parse(fs.readFileSync('./asmrmap/data/data.json', 'utf8'));
-const channelsArr = dataModel.baseData.baseChannels
+const url2 = id => `https://www.googleapis.com/youtube/v3/channels?part=contentDetails&id=${id}&key=${YT_API_TOKEN}`
+const url3 = (id, pageToken = '') => `https://www.googleapis.com/youtube/v3/playlistItems?part=snippet&maxResults=1000&playlistId=${id}&key=${YT_API_TOKEN}&pageToken${pageToken}`
 
 class YT {
-    async getUploadsPlaylist (channelName) {
-        console.log('GETTING PLAYLISTS FOR', channelName);
-        return await axios(url2(channelName)).then(e => {
-            console.log('GOT PLAYLISTS FOR', channelName, e.data.items);
-            e.data.items.forEach(e => {
-                const id = e.contentDetails.relatedPlaylists.uploads
-                return id
-                // getVids(id)
-            })
+    dataModel = JSON.parse(fs.readFileSync('./asmrmap/data/data.json', 'utf8'));
+    channelsArr = this.dataModel.baseData.baseChannels
+
+    async run() {
+        const newChannelsData = this.channelsArr.map(async e => {
+            const playlists = await axios(url2(e.channelId)).then(e => e.data.items)
+            // console.log(playlists);
+            return await e
         })
+        console.log('newChannelsData', newChannelsData)
+        return await newChannelsData
+        // fs.writeFileSync('./asmrmap/data/newdata.json', 'asdasd11232123')
     }
-    async getVids(id) {
-        return await axios(url3(id)).then(e => {
-            return e.data.items
-            // console.log(e.data);
-            // console.log(e.data.nextPageToken);
-            // console.log('ITEMS:');
-            e.data.items.forEach(e => {
-                // console.log(e.snippet.title)
-                // console.log(e.snippet.description)
-            })
-            // console.log(e.data.items.length);
-        })
-    }
+
+    // async getUploadsPlaylist (channelId) {
+    //     console.log('GETTING PLAYLISTS FOR channel: ', channelId);
+
+    //     return await axios(url2(channelId))
+    // }
+    // async getVids(id) {
+    //     console.log('GETTING VIDEOS FROM PLAYLIST ID OF CHANNEL ID: ', id)
+    //     // setInterval(function(e){
+
+    //     // }, 1500)
+    //     console.log('this got vids', this.getVids);
+    //     return await axios(url3(id)).then(e => {
+    //         // console.log('GOT VIDS items', e.data)
+    //         console.log('GOT VIDS next page token', e.data.nextPageToken)
+    //         console.log('GOT VIDS page info', e.data.pageInfo)
+    //         return e.data.items
+    //         e.data.items.forEach(e => {
+    //             // console.log(e.snippet.title)
+    //             // console.log(e.snippet.description)
+    //         })
+    //         // console.log(e.data.items.length);
+    //     })
+    // }
 }
 
+console.log(new YT().run())
+// new YT().run().then(e => { console.log(e) })
+// Promise.all(new YT().run())
 
-const program = async function(){
-    console.log("read channels from file");
-    channelsArr.forEach(({ channelName }) => {
-        new YT().getUploadsPlaylist(channelName)
-    })
-
-    console.log("write new file with channels playlists");
-    console.log("write new file with channels full video's info");
-    fs.writeFileSync('./asmrmap/data/newdata.json', 'asdasd11232123')
-}
-
-program()
 
 // TODO "подгрузить youtube api"
 // TODO "пропарсить каналы"
