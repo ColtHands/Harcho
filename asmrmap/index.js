06946c388e4dbf96653a9b35dce0978c4f932b37
F1ks3r <f1ks3r@yandex.ru>
2020-08-16 19:04:53 +0300

    save

--- asmrmap/index.js
+++ asmrmap/index.js
@@ -3,21 +3,56 @@ const fs = require('fs')
 
 class YT {
     YT_API_TOKEN = 'AIzaSyA2p8emllzwOt7hs2EwSnJr-VqRXMoCT-o'
-    dataModel = JSON.parse(fs.readFileSync('./asmrmap/data/data.json', 'utf8'));
-    channelsArr = this.dataModel.baseData.baseChannels
+    channels = [
+        {
+            "channelName": "Rambalac",
+            "channelId": "UCAcsAE1tpLuP3y7UhxUoWpQ",
+            "playlistIds": [],
+            "videos": []
+        },
+        {
+            "channelName": "Nomadic Ambience",
+            "channelId": "UCqRTj-Nu_8to3jIBlXptOtA",
+            "playlistIds": [],
+            "videos": []
+        },
+        {
+            "channelName": "TokyoStreetView - Japan The Beautiful",
+            "channelId": "UCAxMEpfzdJ2dkrplSWehgcA",
+            "playlistIds": [],
+            "videos": []
+        },
+        {
+            "channelName": "Tokyo Explorer",
+            "channelId": "UCHWmg1FyYtFRiClvHkB-NVA",
+            "playlistIds": [],
+            "videos": []
+        },
+        {
+            "channelName": "Jesse Yang",
+            "channelId": "UCUHwUoMFhNy7wRGbwU4vaGw",
+            "playlistIds": [],
+            "videos": []
+        }
+    ]
     channelContentUrl = id => `https://www.googleapis.com/youtube/v3/channels?part=contentDetails&id=${id}&key=${this.YT_API_TOKEN}`
     playlistItemsUrl = (id, pageToken = '') => `https://www.googleapis.com/youtube/v3/playlistItems?part=snippet&maxResults=12345&playlistId=${id}&key=${this.YT_API_TOKEN}&pageToken=${pageToken}`
 
-    async getAllVidsWithInfo() {
-        const channelsWithPlaylistIds = await this.getUploadsPlaylist(this.channelsArr)
+    run() {
+        this.getAllVidsWithInfo()
+    }
+
+    async getAllVidsWithInfo(channelsArr = this.channels) {
+        const channelsWithPlaylistIds = await this.getUploadsPlaylist(channelsArr)
         const channelsWithVids = await this.getVids(channelsWithPlaylistIds)
-        console.log('channelsWithVids', channelsWithVids);
-        fs.writeFileSync('./asmrmap/data/channelsWithVids.json', JSON.stringify({ channelsWithVids }), 'utf-8')
+
+        fs.writeFileSync('./asmrmap/data/data.json', JSON.stringify({ channelsWithVids }), 'utf-8')
     }
 
     async getUploadsPlaylist(channelsArr) {
         return await Promise.all(channelsArr.map(async e => {
             return axios(this.channelContentUrl(e.channelId)).then(res => {
+                console.log(res.data);
                 const playlistIds = res.data.items.map(e => e.contentDetails.relatedPlaylists.uploads)
                 return { ...e, playlistIds }
             })
@@ -41,15 +76,13 @@ class YT {
 
         return await Promise.all(channelsWithPlaylistIds.map(async e =>  await getVidsWithPage(e) ))
     }
+}
 
-    run() {
-        const asd = JSON.parse(fs.readFileSync('./asmrmap/data/channelsWithVids.json', 'utf8'))
-        console.log(asd)
-    }
+class Spotify {
+
 }
 
 new YT().run()
-
 
 // TODO "подгрузить youtube api"
 // TODO "пропарсить каналы"
